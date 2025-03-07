34eff84b782d45595ed9be36b5828dd703584494
feederbox826 <feederbox826@users.noreply.github.com>
2025-03-07 01:45:21 -0500

    add clipboard sanitization on copy

---
+++ docs/inventory/clipboard.js
@@ -0,0 +1,15 @@
+// win-1252 conversion from https://stackoverflow.com/a/73127563
+const cleanFileName = (filename) =>
+  filename
+    .trim()
+    .replace(/\./g, "")
+    .replace(/\:/g, "-")
+    .replace(/ |\/|\\/g, "_")
+
+const handleClipboard = (evt) => {
+  navigator.clipboard.readText()
+    .then(text => navigator.clipboard.writeText(cleanFileName(text)))
+  evt.preventDefault()
+}
+
+document.addEventListener("copy", handleClipboard)
